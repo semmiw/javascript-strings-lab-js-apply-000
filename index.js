7ae9f3d601c20d3afe1062937e095ca96055767e
Semhar Woldeyesus <semmi.semmiw@gmail.com>
2018-11-02 10:04:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = 'Let's talk about space.';
-
+var topic = "Let's talk about space.";
 var conversation = `${topic}`;
